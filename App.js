5ee585ddac78a8c9ce72c9b706d0209d60d41017
ognevdm <ognev.d@gmail.com>
2021-11-28 09:03:26 +0200

    Move Root Navigator to the file

--- App.js
+++ App.js
@@ -25,6 +25,7 @@ import ClientPage from './screens/ClientPage';
 import InputPage from './screens/InputPage';
 import InputPage_useState from './screens/InputPage_useState';
 import Registration from './screens/Registration'
+import RootNavigator from './navigation/RootNavigator';
 
 // 
 // Reducer
@@ -75,121 +76,121 @@ export default function App({ navigation }) {
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <NavigationContainer>
-            <RootNavigatorConst/>
+            <RootNavigator/>
           </NavigationContainer>
         </PersistGate>
       </Provider>
     );
   }
 }
-const RootNavigatorConst = () => {
-  const userIsSignedUp = useSelector(state => state.profile.signedUp);
-  return (
-  <Stack.Navigator>
-            {!userIsSignedUp && (
-              <Stack.Screen
-                name="Registration"
-                component={Registration}
-                options={{
-                  title: 'Регистрация',
-                  headerStyle: {
-                    height: 80,
-                  },
-                  headerTintColor: '#000000',
-                  headerTitleStyle: {
-                    fontFamily: 'Roboto_500Medium',
-                    fontSize: 18,
-                    fontWeight: '500',
-                  },
-                  headerBackTitleVisible: false,
-                }}
-              />
-            )}
-            <Stack.Screen
-              name="Клиенты"
-              component={Clients}
-              options={{
-                title: 'Клиенты',
-                headerStyle: {
-                  backgroundColor: '#E02329',
-                  borderTopColor: '#b11f27',
-                  borderTopWidth: 35,
-                  height: 80,
-                  // padding: 100,
-                },
-                headerTintColor: '#fff',
-                headerTitleStyle: {
-                  fontFamily: 'Roboto_500Medium',
-                  fontSize: 20,
-                  fontWeight: '500',
-                },
-              }}
-            />
-            <Stack.Screen
-              style={{ backgroundColor: '#b11f27' }}
-              name="ClientPage"
-              component={ClientPage}
-              options={{
-                title: 'О клиенте',
-                headerStyle: {
-                  backgroundColor: '#E02329',
-                  borderTopColor: '#b11f27',
-                  borderTopWidth: 35,
-                  height: 80,
-                  // padding: 100,
-                },
-                headerTintColor: '#fff',
-                headerTitleStyle: {
-                  fontFamily: 'Roboto_500Medium',
-                  fontSize: 20,
-                  fontWeight: '500',
-                },
-              }}
-            />
-            <Stack.Screen
-              name="InputPage"
-              component={InputPage}
-              options={{
-                title: 'Добавить Клиента',
-                headerStyle: {
-                  backgroundColor: '#E02329',
-                  borderTopColor: '#b11f27',
-                  borderTopWidth: 35,
-                  height: 80,
-                  // padding: 100,
-                },
-                headerTintColor: '#fff',
-                headerTitleStyle: {
-                  fontFamily: 'Roboto_500Medium',
-                  fontSize: 20,
-                  fontWeight: '500',
-                },
-                headerBackTitleVisible: false,
-              }}
-            />
-            <Stack.Screen
-              name="InputPage_useState"
-              component={InputPage_useState}
-              options={{
-                title: 'Добавить Клиента',
-                headerStyle: {
-                  backgroundColor: '#E02329',
-                  borderTopColor: '#b11f27',
-                  borderTopWidth: 35,
-                  height: 80,
-                  // padding: 100,
-                },
-                headerTintColor: '#fff',
-                headerTitleStyle: {
-                  fontFamily: 'Roboto_500Medium',
-                  fontSize: 20,
-                  fontWeight: '500',
-                },
-                headerBackTitleVisible: false,
-              }}
-            />
+// const RootNavigatorConst = () => {
+//   const userIsSignedUp = useSelector(state => state.profile.signedUp);
+//   return (
+//   <Stack.Navigator>
+//             {!userIsSignedUp && (
+//               <Stack.Screen
+//                 name="Registration"
+//                 component={Registration}
+//                 options={{
+//                   title: 'Регистрация',
+//                   headerStyle: {
+//                     height: 80,
+//                   },
+//                   headerTintColor: '#000000',
+//                   headerTitleStyle: {
+//                     fontFamily: 'Roboto_500Medium',
+//                     fontSize: 18,
+//                     fontWeight: '500',
+//                   },
+//                   headerBackTitleVisible: false,
+//                 }}
+//               />
+//             )}
+//             <Stack.Screen
+//               name="Клиенты"
+//               component={Clients}
+//               options={{
+//                 title: 'Клиенты',
+//                 headerStyle: {
+//                   backgroundColor: '#E02329',
+//                   borderTopColor: '#b11f27',
+//                   borderTopWidth: 35,
+//                   height: 80,
+//                   // padding: 100,
+//                 },
+//                 headerTintColor: '#fff',
+//                 headerTitleStyle: {
+//                   fontFamily: 'Roboto_500Medium',
+//                   fontSize: 20,
+//                   fontWeight: '500',
+//                 },
+//               }}
+//             />
+//             <Stack.Screen
+//               style={{ backgroundColor: '#b11f27' }}
+//               name="ClientPage"
+//               component={ClientPage}
+//               options={{
+//                 title: 'О клиенте',
+//                 headerStyle: {
+//                   backgroundColor: '#E02329',
+//                   borderTopColor: '#b11f27',
+//                   borderTopWidth: 35,
+//                   height: 80,
+//                   // padding: 100,
+//                 },
+//                 headerTintColor: '#fff',
+//                 headerTitleStyle: {
+//                   fontFamily: 'Roboto_500Medium',
+//                   fontSize: 20,
+//                   fontWeight: '500',
+//                 },
+//               }}
+//             />
+//             <Stack.Screen
+//               name="InputPage"
+//               component={InputPage}
+//               options={{
+//                 title: 'Добавить Клиента',
+//                 headerStyle: {
+//                   backgroundColor: '#E02329',
+//                   borderTopColor: '#b11f27',
+//                   borderTopWidth: 35,
+//                   height: 80,
+//                   // padding: 100,
+//                 },
+//                 headerTintColor: '#fff',
+//                 headerTitleStyle: {
+//                   fontFamily: 'Roboto_500Medium',
+//                   fontSize: 20,
+//                   fontWeight: '500',
+//                 },
+//                 headerBackTitleVisible: false,
+//               }}
+//             />
+//             <Stack.Screen
+//               name="InputPage_useState"
+//               component={InputPage_useState}
+//               options={{
+//                 title: 'Добавить Клиента',
+//                 headerStyle: {
+//                   backgroundColor: '#E02329',
+//                   borderTopColor: '#b11f27',
+//                   borderTopWidth: 35,
+//                   height: 80,
+//                   // padding: 100,
+//                 },
+//                 headerTintColor: '#fff',
+//                 headerTitleStyle: {
+//                   fontFamily: 'Roboto_500Medium',
+//                   fontSize: 20,
+//                   fontWeight: '500',
+//                 },
+//                 headerBackTitleVisible: false,
+//               }}
+//             />
 
-          </Stack.Navigator>
-          )
-}
+//           </Stack.Navigator>
+//           )
+// }
 
--- navigation/RootNavigator.js
+++ navigation/RootNavigator.js
@@ -13,110 +13,111 @@ import Registration from '../screens/Registration'
 export default function RootNavigator()
 {
   const Stack = createStackNavigator();
-  const userProfile = useSelector(state => state.profile);
-    <Stack.Navigator>
-              {!userProfile && (
-                <Stack.Screen
-                  name="Registration"
-                  component={Registration}
-                  options={{
-                    title: 'Регистрация',
-                    headerStyle: {
-                      height: 80,
-                    },
-                    headerTintColor: '#000000',
-                    headerTitleStyle: {
-                      fontFamily: 'Roboto_500Medium',
-                      fontSize: 18,
-                      fontWeight: '500',
-                    },
-                    headerBackTitleVisible: false,
-                  }}
-                />
-              )}
-              <Stack.Screen
-                name="Клиенты"
-                component={Clients}
-                options={{
-                  title: 'Клиенты',
-                  headerStyle: {
-                    backgroundColor: '#E02329',
-                    borderTopColor: '#b11f27',
-                    borderTopWidth: 35,
-                    height: 80,
-                    // padding: 100,
-                  },
-                  headerTintColor: '#fff',
-                  headerTitleStyle: {
-                    fontFamily: 'Roboto_500Medium',
-                    fontSize: 20,
-                    fontWeight: '500',
-                  },
-                }}
-              />
-              <Stack.Screen
-                style={{ backgroundColor: '#b11f27' }}
-                name="ClientPage"
-                component={ClientPage}
-                options={{
-                  title: 'О клиенте',
-                  headerStyle: {
-                    backgroundColor: '#E02329',
-                    borderTopColor: '#b11f27',
-                    borderTopWidth: 35,
-                    height: 80,
-                    // padding: 100,
-                  },
-                  headerTintColor: '#fff',
-                  headerTitleStyle: {
-                    fontFamily: 'Roboto_500Medium',
-                    fontSize: 20,
-                    fontWeight: '500',
-                  },
-                }}
-              />
-              <Stack.Screen
-                name="InputPage"
-                component={InputPage}
-                options={{
-                  title: 'Добавить Клиента',
-                  headerStyle: {
-                    backgroundColor: '#E02329',
-                    borderTopColor: '#b11f27',
-                    borderTopWidth: 35,
-                    height: 80,
-                    // padding: 100,
-                  },
-                  headerTintColor: '#fff',
-                  headerTitleStyle: {
-                    fontFamily: 'Roboto_500Medium',
-                    fontSize: 20,
-                    fontWeight: '500',
-                  },
-                  headerBackTitleVisible: false,
-                }}
-              />
-              <Stack.Screen
-                name="InputPage_useState"
-                component={InputPage_useState}
-                options={{
-                  title: 'Добавить Клиента',
-                  headerStyle: {
-                    backgroundColor: '#E02329',
-                    borderTopColor: '#b11f27',
-                    borderTopWidth: 35,
-                    height: 80,
-                    // padding: 100,
-                  },
-                  headerTintColor: '#fff',
-                  headerTitleStyle: {
-                    fontFamily: 'Roboto_500Medium',
-                    fontSize: 20,
-                    fontWeight: '500',
-                  },
-                  headerBackTitleVisible: false,
-                }}
-              />
+  const userIsSignedUp = useSelector(state => state.profile.signedUp);
+   return( <Stack.Navigator>
+    {!userIsSignedUp && (
+      <Stack.Screen
+        name="Registration"
+        component={Registration}
+        options={{
+          title: 'Регистрация',
+          headerStyle: {
+            height: 80,
+          },
+          headerTintColor: '#000000',
+          headerTitleStyle: {
+            fontFamily: 'Roboto_500Medium',
+            fontSize: 18,
+            fontWeight: '500',
+          },
+          headerBackTitleVisible: false,
+        }}
+      />
+    )}
+    <Stack.Screen
+      name="Клиенты"
+      component={Clients}
+      options={{
+        title: 'Клиенты',
+        headerStyle: {
+          backgroundColor: '#E02329',
+          borderTopColor: '#b11f27',
+          borderTopWidth: 35,
+          height: 80,
+          // padding: 100,
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontFamily: 'Roboto_500Medium',
+          fontSize: 20,
+          fontWeight: '500',
+        },
+      }}
+    />
+    <Stack.Screen
+      style={{ backgroundColor: '#b11f27' }}
+      name="ClientPage"
+      component={ClientPage}
+      options={{
+        title: 'О клиенте',
+        headerStyle: {
+          backgroundColor: '#E02329',
+          borderTopColor: '#b11f27',
+          borderTopWidth: 35,
+          height: 80,
+          // padding: 100,
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontFamily: 'Roboto_500Medium',
+          fontSize: 20,
+          fontWeight: '500',
+        },
+      }}
+    />
+    <Stack.Screen
+      name="InputPage"
+      component={InputPage}
+      options={{
+        title: 'Добавить Клиента',
+        headerStyle: {
+          backgroundColor: '#E02329',
+          borderTopColor: '#b11f27',
+          borderTopWidth: 35,
+          height: 80,
+          // padding: 100,
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontFamily: 'Roboto_500Medium',
+          fontSize: 20,
+          fontWeight: '500',
+        },
+        headerBackTitleVisible: false,
+      }}
+    />
+    <Stack.Screen
+      name="InputPage_useState"
+      component={InputPage_useState}
+      options={{
+        title: 'Добавить Клиента',
+        headerStyle: {
+          backgroundColor: '#E02329',
+          borderTopColor: '#b11f27',
+          borderTopWidth: 35,
+          height: 80,
+          // padding: 100,
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontFamily: 'Roboto_500Medium',
+          fontSize: 20,
+          fontWeight: '500',
+        },
+        headerBackTitleVisible: false,
+      }}
+    />
 
-            </Stack.Navigator>
+  </Stack.Navigator>
+            )
 }
